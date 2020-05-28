9de299d550083d4fae95c5f9d7c29a9f98641521
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-05-28 19:31:16 +0300

    getRandomIndex function added, quotes and authors showed in p tags,onClick event added

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,46 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [quotes, setQuotes] = useState([]);
+  const [quotes, setQuotes] = useState([
+    {
+      quote:
+        "Life isn’t about getting and having, it’s about giving and being.",
+      author: "Kevin Kruse",
+    },
+  ]);
+  const [index, setIndex] = useState(0);
 
   const fetchQuotes = async () => {
     const response = await fetch(
       "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     );
     const json = await response.json();
-    console.log(json.quotes[0]);
-    setQuotes(json.quotes[0]);
+    setQuotes(json.quotes);
   };
   useEffect(() => {
     fetchQuotes();
   }, []);
 
+  const getRandomIndex = () => {
+    const index = Math.floor(Math.random() * 102);
+    setIndex(index);
+  };
+  const quote = quotes[index];
+
   return (
     <div id="quote-box">
-      <p id="text">
-        <i className="fas fa-quote-left"></i>
-        {quotes.quote}
-      </p>
-      <p id="author">{quotes.author}</p>
-      <button id="new-quote">New Quote</button>
+      {quote && (
+        <div>
+          <p id="text">
+            <i className="fas fa-quote-left"></i>
+            {quote.quote}
+          </p>
+          <p id="author">{quote.author}</p>
+        </div>
+      )}
+      <button id="new-quote" onClick={getRandomIndex}>
+        New Quote
+      </button>
       <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
         <i className="fab fa-twitter-square"></i>
       </a>
